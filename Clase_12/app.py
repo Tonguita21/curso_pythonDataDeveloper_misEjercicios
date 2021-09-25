07538d01f6f0ca7c735884de9b9215ff3733a3ee
Gaston Roces <GRoces@galiciaseguros.com.ar>
2021-09-25 08:43:44 -0300

    Agregamos validaciones en la api

--- Clase_12/app.py
+++ Clase_12/app.py
@@ -15,30 +15,43 @@ app = Flask(__name__)
 client = MongoClient("mongodb+srv://eant-groces:Gaston21@pdd-lm-n-297.r4bmn.mongodb.net/api-twitter?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 
 
-@app.route("/tweets")
-def getTweets():
-    twitter = client['api-twitter']['tweets']
-    
-    users = twitter.find()
-    # users = twitter.find().limit(2)  -- En el caso de querer limitar
-    
-    # for user in users:
-    #     print('------------------')
-    #     print(user)
-    # return "mira la consola..."
-    
-    result = []
-    
-    for user in users:
-        item = { 'usuario': user['name']}
-        result.append(item)
-                
-    
-    return app.response_class(response = json.dumps(result), status = 200 , mimetype = "application/json")
-  # response = app.response_class(response = json.dumps(users), status = 200 , mimetype = "application/json") 
-        
-    # return response
+# -- Agregamos la ruta tweets 
+# @app.route("/tweets")
 
+# -- Podemos configurar la ruta para que sea dinamica, reciba parametros
+# -- y el mismo se envie a la funcion para tener logica en base al mismo
+@app.route("/tweets/<param1>")
+
+def getTweets(param1):
+    
+    twitter = client['api-twitter']['tweets']
+ 
+    # users = twitter.find()
+    
+    # -- Agregamos la validacion del parametro para que esté dentro de los parametros esperados.
+    # -- Caso contrario se informa Error
+    if param1 not in ['people','company']:
+        result = {'error':'categoria no disponible'}
+    else:
+        
+        # -- Podemos pasarle el parametro para filtrar la informacion.
+        users = twitter.find({'type' : param1})
+        # users = twitter.find().limit(2)  -- En el caso de querer limitar
+        
+        # for user in users:
+        #     print('------------------')
+        #     print(user)
+        # return "mira la consola..."
+        
+        result = []
+        
+        for user in users:
+            item = { 'usuario': user['name']}
+            result.append(item)
+    
+    # -- Se retorna el valor de result en formato json            
+    return app.response_class(response = json.dumps(result), status = 200 , mimetype = "application/json")
+  
 
 # -- armamos la ruta de la app
 # -- La contrabarra indica que es el directorio raiz 
